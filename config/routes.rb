84ee44d24ed24b4e0c388e9b6f77c6f1012587de
IPIvliev <cccp1945@mail.ru>
2014-03-31 19:38:59 +0400

    Some changes in design

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,13 @@ Dorogaminina::Application.routes.draw do
 
   resources :zvenos
 
+# Проплата
+  scope 'robokassa' do
+    match 'paid'    => 'robokassa#paid',    :as => :robokassa_paid # to handle Robokassa push request
 
-
+    match 'success' => 'robokassa#success', :as => :robokassa_success # to handle Robokassa success redirect
+    match 'fail'    => 'robokassa#fail',    :as => :robokassa_fail # to handle Robokassa fail redirect
+  end
 
 # Статические страницы
   get "static_pages/index"
