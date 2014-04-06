5d3310fd6989ed4965505a34676dd7c9c1a043a1
Razrus <cccp1945@mail.ru>
2014-04-06 13:50:46 +0400

    Play with pay

--- app/controllers/robokassa_controller.rb
+++ app/controllers/robokassa_controller.rb
@@ -4,7 +4,7 @@ class RobokassaController < ApplicationController
 
   include ActiveMerchant::Billing::Integrations
 
-  skip_before_filter :verify_authenticity_token # skip before filter if you chosen POST request for callbacks
+  #skip_before_filter :verify_authenticity_token # skip before filter if you chosen POST request for callbacks
 
   before_filter :create_notification
   before_filter :find_payment
@@ -19,9 +19,9 @@ class RobokassaController < ApplicationController
     end
   end
 
-  # Robokassa redirect user to this action if it’s all ok
+  # Robokassa redirect user to this action if it’s all ok !@user.approved? &&
   def success
-    if !@user.approved? && @notification.acknowledge
+    if  @notification.acknowledge
       @user.approve!
     end
 
