74118948a3976380a9352c1259c970b080d41dbe
Razrus <cccp1945@mail.ru>
2014-04-14 00:58:07 +0400

    Change ix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "XI Открытый велопробег Дорога Минина"
+    base_title = "IX Открытый велопробег Дорога Минина"
     if page_title.empty?
       base_title
     else
