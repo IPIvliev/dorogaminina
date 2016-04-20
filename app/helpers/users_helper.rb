7f75f53d48c5cf37ddbd61817e0496cb53f59296
IPIvliev <cccp1945@mail.ru>
2016-04-20 15:39:53 +0300

    Changes for DM 2016

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -12,4 +12,19 @@ module UsersHelper
 				return "<strong class='green_pay'>Оплачен</strong>".html_safe
 		end
 	end
+
+	def dostavka(tochka)
+		case tochka
+			when 0
+				return "Без доставки".html_safe
+			when 1
+				return "Московский вокзал".html_safe
+			when 2
+				return "Индиго Лайф".html_safe
+			when 3
+				return "Щербинки 2".html_safe
+			when 4
+				return "Парк Культуры".html_safe
+		end
+	end	
 end
